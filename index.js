b3e5f787b262c5526e0149102e502d25a7cd2655
Vaska Baltoska <vaska.v.83@gmail.com>
2024-06-19 14:57:09 +0200

    homework3

---
+++ index.js
@@ -0,0 +1,47 @@
+let str = "learning javascript String Methods";
+
+console.log(str.length);
+
+console.log(str.charAt(0));
+
+console.log(str.charCodeAt(2));
+
+console.log(str.concat(" ", "is", " ", "awesome"));
+
+console.log(str.includes("String"));
+
+console.log(str.includes("hello"));
+
+console.log(str.endsWith("g"));
+
+console.log(str.endsWith("methods"));
+
+console.log(str.indexOf("String"));
+
+console.log(str.indexOf("Javascript"));
+
+console.log(str.lastIndexOf("String"));
+
+console.log(str.lastIndexOf("Javascript"));
+
+console.log(str.replace("javascript ", "JS"));
+
+console.log(str.replaceAll("a", "A"));
+
+console.log(str.slice(0, 10));
+
+console.log(str.split(" "));
+
+console.log(str.startsWith("l"));
+
+console.log(str.toLowerCase());
+
+console.log(str.toUpperCase());
+
+console.log(str.trim());
+
+console.log(str.trimStart());
+
+console.log(str.trimEnd());
+
+console.log(str.repeat(5));
